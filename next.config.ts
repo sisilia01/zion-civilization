fc8139d98a385f61a6607507caacf0916c2846de
ZION-Civilization <zioncivilization@gmail.com>
2026-05-19 15:51:47 +0000

    ZionBet: SUI logo fixed, 8 tabs final, gold borders DeepBook cards

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,18 @@ const nextConfig: NextConfig = {
         hostname: "cryptologos.cc",
         pathname: "/logos/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.prod.website-files.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s2.coinmarketcap.com",
+      },
     ],
   },
   async headers() {
@@ -28,7 +40,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data: https://polymarket-upload.s3.us-east-2.amazonaws.com https://cryptologos.cc;",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data: https://polymarket-upload.s3.us-east-2.amazonaws.com https://cryptologos.cc https://cdn.prod.website-files.com https://assets.coingecko.com https://s2.coinmarketcap.com https://raw.githubusercontent.com;",
           },
         ],
       },
