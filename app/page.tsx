a7b4d9b3f7e380dbbaf63840f0591cc2cbaad311
ZION-Civilization <zioncivilization@gmail.com>
2026-06-07 09:40:32 +0000

    Perfect 360 space: ESO Milky Way panorama 6000x3000 + city lights by population

--- app/page.tsx
+++ app/page.tsx
@@ -976,6 +976,7 @@ function DistrictMapPanel() {
         <LivingPlanet
           prosperity={prosperity}
           revolution={civilizationData.revolution ?? 0}
+          population={civilizationData.population ?? 0}
           civilizationData={civilizationData}
           height={400}
         />
